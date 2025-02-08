213786c48dbb1eeaa8d2b34554d0da1f3fec8630
Влад <killyourselff1703@mail.ru>
2025-02-08 14:41:34 +0400

    feat: added storage's function

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,11 @@
 package storage
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"time"
+	"to-do-list/internal/task"
 )
 
 type Storage struct {
@@ -14,9 +16,12 @@ func New(storagePath string) (*Storage, error) {
 	const op = "storage.sqlite.NewStorage"
 
 	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
 
-	stmt, err := db.Prepare(`
-    CREATE TABLE tasks (
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS tasks (
     	id INTEGER PRIMARY KEY AUTOINCREMENT,
     	user_id INTEGER NOT NULL,
     	name TEXT NOT NULL,
@@ -24,12 +29,6 @@ func New(storagePath string) (*Storage, error) {
     	due_date DATETIME,                    
     	created_at DATETIME DEFAULT CURRENT_TIMESTAMP);
     `)
-
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-
-	_, err = stmt.Exec()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
@@ -37,16 +36,11 @@ func New(storagePath string) (*Storage, error) {
 	return &Storage{db: db}, nil
 }
 
-func (s *Storage) AddTask(userId int64, taskName string, taskDescription string, dueDate time.Time) (int, error) {
+func (s *Storage) AddTask(ctx context.Context, userId int64, taskName, taskDescription string, dueDate time.Time) (int, error) {
 	const op = "storage.sqlite.AddTask"
 
-	stmt, err := s.db.Prepare(`insert into tasks(user_id ,name, description, due_date) values(?, ?, ?, ?)`)
-
-	if err != nil {
-		return 0, fmt.Errorf("%s: %w", op, err)
-	}
-
-	res, err := stmt.Exec(userId, taskName, taskDescription, dueDate)
+	res, err := s.db.ExecContext(ctx, `INSERT INTO tasks(user_id, name, description, due_date) VALUES (?, ?, ?, ?)`,
+		userId, taskName, taskDescription, dueDate)
 	if err != nil {
 		return 0, fmt.Errorf("%s: %w", op, err)
 	}
@@ -57,4 +51,51 @@ func (s *Storage) AddTask(userId int64, taskName string, taskDescription string,
 	}
 
 	return int(lastID), nil
+}
+
+func (s *Storage) GetTasks(ctx context.Context, userId int64) ([]task.Task, error) {
+	const op = "storage.sqlite.GetTasks"
+
+	rows, err := s.db.QueryContext(ctx, `SELECT id, user_id, name, description, due_date, created_at FROM tasks WHERE user_id = ?`, userId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	defer rows.Close()
+
+	var tasks []task.Task
+	for rows.Next() {
+		var t task.Task
+		err := rows.Scan(&t.Id, &t.UserId, &t.Name, &t.Description, &t.DueDate, &t.CreatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		tasks = append(tasks, t)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return tasks, nil
+}
+
+func (s *Storage) DeleteTask(ctx context.Context, userId int64, taskName string) error {
+	const op = "storage.sqlite.DeleteTask"
+
+	_, err := s.db.ExecContext(ctx, `DELETE FROM tasks WHERE user_id = ? AND name = ?`, userId, taskName)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *Storage) ChangeTask(ctx context.Context, taskId int, newName string, newDescription string, newDueDate time.Time) error {
+	const op = "storage.sqlite.ChangeTask"
+
+	_, err := s.db.ExecContext(ctx, "UPDATE tasks SET name = ?, description = ?, due_date = ? WHERE id = ?", newName, newDescription, newDueDate, taskId)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
